d2cb2541614647990ad1910105a5abf99fc7e1f0
dennis.xiao <dennis.xiao@ladbrokes.com.au>
2022-03-15 13:45:59 +1000

    Refactor useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,32 +1,110 @@
-import { useState, useEffect } from "react";
+import { useEffect, useRef, useReducer } from "react";
 
 export enum FETCH_STATUS {
   IDLE = "IDLE",
+  FETCH_ERROR = "FETCH_ERROR",
   FETCHING = "FETCHING",
   FETCHED = "FETCHED",
 }
 
+const initialState = {
+  status: FETCH_STATUS.IDLE,
+  error: null,
+  data: [],
+};
+
+export type InitialState = typeof initialState;
+
+export type ActionFetching = {
+  type: FETCH_STATUS.FETCHING;
+};
+
+export type ActionFetched = {
+  type: FETCH_STATUS.FETCHED;
+  payload: {
+    data: any;
+  };
+};
+
+export type ActionError = {
+  type: FETCH_STATUS.FETCH_ERROR;
+  payload: {
+    error: string;
+  };
+};
+
+export type FetchAction = ActionFetching | ActionFetched | ActionError;
+
 export const useFetch = (url: string) => {
-  const [status, setStatus] = useState(FETCH_STATUS.IDLE);
-  const [data, setData] = useState([]);
+  const cache = useRef({});
+
+  // Use `useReducer` so we can control 
+  const [state, dispatch] = useReducer(
+    (state: InitialState, action: FetchAction) => {
+      switch (action.type) {
+        case FETCH_STATUS.FETCHING:
+          return { ...initialState, status: FETCH_STATUS.FETCHING };
+
+        case FETCH_STATUS.FETCHED:
+          return {
+            ...initialState,
+            status: FETCH_STATUS.FETCHED,
+            data: action.payload.data,
+          };
+
+        case FETCH_STATUS.FETCH_ERROR:
+          return {
+            ...initialState,
+            status: FETCH_STATUS.FETCH_ERROR,
+            error: action.payload.error,
+          };
+
+        default:
+          return state;
+      }
+    },
+    initialState
+  );
 
   useEffect(() => {
-    if (!url) return;
+    let cancelRequest = false;
+
+    if (!url || !url.trim()) return;
 
     const fetchData = async () => {
-      setStatus(FETCH_STATUS.FETCHING);
+      dispatch({ type: FETCH_STATUS.FETCHING });
 
-      const response = await fetch(url);
-      const data = await response.json();
+      // Return cached result if we have fetched the URL before
+      if (cache.current[url]) {
+        const data = cache.current[url];
+        dispatch({ type: FETCH_STATUS.FETCHED, payload: data });
+      } else {
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
 
-      setData(data);
-      setStatus(FETCH_STATUS.FETCHED);
+          // Update cache object
+          cache.current[url] = data;
+
+          if (cancelRequest) return;
+
+          dispatch({ type: FETCH_STATUS.FETCHED, payload: data });
+        } catch (error) {
+          if (cancelRequest) return;
+
+          dispatch({ type: FETCH_STATUS.FETCH_ERROR, payload: error.message });
+        }
+      }
     };
 
     fetchData();
+
+    return function cleanup() {
+      cancelRequest = true;
+    };
   }, [url]);
 
-  return { status, data };
+  return state;
 };
 
 /**
